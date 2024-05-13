ec86324812add5b4513e444e2743e1ee718e1b7b
Anwar Hossain <irishkhan33@gmail.com>
2024-05-13 12:31:12 +0600

    multer setup addaed

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const {setupRoutes} = require('./modules/video/controller')
 const app = express()
 const port = 4000
 app.use(express.json())
 app.listen(port, () => {
+    setupRoutes(app)
     console.log(`Example app listening on port ${port}`)
   })
 module.exports = app
---
+++ server/src/modules/video/controller.js
@@ -0,0 +1,53 @@
+const setupRoutes = (app) => {
+  app.get("/", async (req, res) => {
+    return res.status(200).send({ message: "Success" });
+  });
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/videos");
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, file.fieldname + "-" + uniqueSuffix);
+    },
+  });
+
+  const fileFilter = (req, file, cb) => {
+    // The function should call `cb` with a boolean
+    // to indicate if the file should be accepted
+
+    // To reject this file pass `false`, like so:
+   
+    if (file.mimetype === "video/mp4" || file.mimetype === "video/x-matroska") {
+    // To accept the file pass `true`, like so:
+      console.log("File type supported", file);
+      cb(null, true);
+    } else {
+      console.log("File type not supported", file);
+      // You can always pass an error if something goes wrong:
+      cb(new multer.MulterError("File type not supported"), false);
+    }
+  };
+
+  const upload = multer({
+    dest: "uploads/videos",
+    fileFilter: fileFilter,
+    limits: { fileSize: 50000000 },
+    storage: storage,
+  }).single("video");
+
+  const uploadProcessor = (req, res, next) => {
+    upload(req, res, (err) => {
+      if (err) {
+        res.status(400).json({ status: "error", message: err });
+        return;
+      } else {
+        console.log("file", res.file);
+        next();
+      }
+    });
+  };
+  app.post("/upload", uploadProcessor, async (req, res) => {});
+};
+
+module.exports = { setupRoutes };
