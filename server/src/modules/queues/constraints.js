ec46c51e8e2c943f675f65c6ead6eea20937ab03
anwarhossain1 <irishkhan33@gmail.com>
2024-06-21 11:55:53 +0600

    constraints added for redis

---
+++ server/src/modules/queues/constraints.js
@@ -0,0 +1,10 @@
+const VIDEO_QUEUE_EVENTS = {
+    VIDEO_UPLOADED: 'video.uploaded',
+    VIDEO_PROCESSING: 'video.processing',
+    VIDEO_PROCESSED: 'video.processed',
+    VIDEO_THUMBNAIL_GENERATED: 'video.thumbnail.generated',
+    VIDEO_HLS_CONVERTING: 'video.hls-converting',
+    VIDEO_HLS_CONVERTED: 'video.hls.converted',
+  };
+
+  module.exports = { VIDEO_QUEUE_EVENTS };
